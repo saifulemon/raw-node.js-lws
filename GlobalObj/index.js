5eb143adbf8a8bc00764d1b9f9594a756ff2c86b
saifulemon <463saiful0@gmail.com>
2023-04-13 18:06:08 +0600

    basic module implements

--- GlobalObj/index.js
+++ GlobalObj/index.js
@@ -1,1 +1,3 @@
-console.log("loading...");
+const people = require('./people');
+
+console.log(people);
---
+++ GlobalObj/people.js
@@ -0,0 +1,6 @@
+const people = ['sakib', 'tamim', 'masrafe', 'musfiq'];
+const num = 7;
+function test() {
+    console.log('test');
+}
+module.exports = people;
