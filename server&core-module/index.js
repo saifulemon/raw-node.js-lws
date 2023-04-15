60ca9096c8d50b95219f3cb21841beba2a1e6779
saifulemon <463saiful0@gmail.com>
2023-04-15 13:01:42 +0600

    events module in node.js

--- server&core-module/index.js
+++ server&core-module/index.js
@@ -22,3 +22,16 @@
 // fs.readFile('myfile.txt', (err, data) => {
 //     console.log(data.toString());
 // });
+
+/* Events module */
+const EventEmitter = require('events');
+
+const emitter = new EventEmitter();
+
+// register a listener for bellRing event
+emitter.on('bellRing', () => {
+    console.log('we need to run');
+});
+
+// raise an event
+emitter.emit('bellRing');
