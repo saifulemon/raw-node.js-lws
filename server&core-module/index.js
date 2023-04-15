8f8cf3719bf6c4c7c4a3fd85b52bbb7bdef2ba39
saifulemon <463saiful0@gmail.com>
2023-04-15 14:44:16 +0600

    extending events

--- server&core-module/index.js
+++ server&core-module/index.js
@@ -24,14 +24,26 @@
 // });
 
 /* Events module */
-const EventEmitter = require('events');
+// const EventEmitter = require('events');
 
-const emitter = new EventEmitter();
+// const emitter = new EventEmitter();
+
+// // register a listener for bellRing event
+// emitter.on('bellRing', () => {
+//     console.log('we need to run');
+// });
+
+// // raise an event
+// emitter.emit('bellRing');
+
+/* Events with user defined module */
+const School = require('./school');
+
+const school = new School();
 
 // register a listener for bellRing event
-emitter.on('bellRing', () => {
-    console.log('we need to run');
+school.on('bellRing', ({ period, text }) => {
+    console.log(`We need to run because ${period} ${text}`);
 });
 
-// raise an event
-emitter.emit('bellRing');
+school.startPeriod();
