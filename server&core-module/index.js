1aff14d1daeff0e80a99cdec64bcd8b3dcde86f7
saifulemon <463saiful0@gmail.com>
2023-04-15 11:05:04 +0600

    file system module explored

--- server&core-module/index.js
+++ server&core-module/index.js
@@ -1,4 +1,24 @@
-const path = require('path');
+/* path module */
+// const path = require('path');
+// const myPath = ""
+// console.log(path.parse(myPath));
 
-const myPath = '/home/saifulemon/Desktop/Saiful-Emon/Node.js/raw-node.js-lws/server&core-module/index.js';
-console.log(path.parse(myPath));
+/* os module */
+// const os = require('os');
+// console.log(os.platform());
+
+/* fs module */
+// const fs = require('fs');
+
+// file write example
+// fs.writeFileSync('myfile.txt', 'Hello Node.js!');
+// fs.appendFileSync('myfile.txt', 'how are you?');
+
+// // file read example
+// const data = fs.readFileSync('myfile.txt');
+// console.log(data.toString());
+
+// file read example in asynchronous way
+// fs.readFile('myfile.txt', (err, data) => {
+//     console.log(data.toString());
+// });
