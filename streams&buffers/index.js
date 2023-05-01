44c63da4e8c8156689fdf9fa72e5786a6061da1d
saifulemon <463saiful0@gmail.com>
2023-05-01 15:33:10 +0600

    write stream example

--- streams&buffers/index.js
+++ streams&buffers/index.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
-
+// read stream example
 const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`);
 
+// ourReadStream.on('data', (chunk) => {
+//     console.log(chunk.toString());
+// });
+
+// write stream example
+const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
+
 ourReadStream.on('data', (chunk) => {
-    console.log(chunk.toString());
+    ourWriteStream.write(chunk);
 });
