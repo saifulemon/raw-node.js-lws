517a09af8d085e66d77265c5272a0984cf6c0e50
saifulemon <463saiful0@gmail.com>
2023-05-11 19:43:50 +0600

    crud operation with node core modules

--- UptimeMonitoringAPI/index.js
+++ UptimeMonitoringAPI/index.js
@@ -10,13 +10,30 @@ const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
 const environment = require('./helpers/environments');
 const data = require('./lib/data');
+const { error } = require('console');
 
 // app object - module scafolding
 const app = {};
 
 // testing file system
+// data create
 data.create('test', 'newFile', { name: 'bangladesh', language: 'bangla' }, (err) => {
     console.log('error was', err);
+});
+
+// data read
+data.read('test', 'newFile', (err, data2) => {
+    console.log(err, data2);
+});
+
+// data update
+data.update('test', 'newFile', { name: 'England', language: 'English' }, (err) => {
+    console.log(err);
+});
+
+// data delete
+data.delete('test', 'newFile', (err) => {
+    console.log(err);
 });
 
 // configuration
